75bc03ca1c75f27a3bc05c159327b0bec9c11f6c
Javier Diaz <163352272+NJavierDiaz@users.noreply.github.com>
2024-04-15 20:53:36 -0300

    Tp n5
    Se agrego el trabajo practico n5 "agregar productos con un JComboBox"

---
+++ Tp5/src/Entidades/Producto.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Javi
+ */
+
+public class Producto {
+    private String nombre;
+    private String categoria;
+    private Double precio;
+
+    public Producto(String nombre, String categoria, Double precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public Producto() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Producto{");
+        sb.append("nombre=").append(nombre);
+        sb.append(", categoria=").append(categoria);
+        sb.append(", precio=").append(precio);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+}
